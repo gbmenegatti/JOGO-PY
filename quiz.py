3ac6198f9649a0f32c1d78324d6c83554ce9426e
Juliana Ferreira Evangelista <61571220+JulianaFe21@users.noreply.github.com>
2022-05-02 12:16:32 -0300

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -24,7 +24,7 @@ else: print("Você errou!")
 
 //Pergunta Ciências da Natureza // 
     
-print("PERGUNTA 3 - O processo de urbanização tem provocado o surgimento de inúmeros impactos ambientais. Dentre eles, podem ser citadas: As enchentes urbanas. \n A poluição visual. \n C- A conturbação. \n D) A bicefalia urbana.\n") 
+print("PERGUNTA 3 - O processo de urbanização tem provocado o surgimento de inúmeros impactos ambientais. Dentre eles, podem ser citadas: 1. As enchentes urbanas. \n 2.A poluição visual. \n 3. A conturbação. \n 4. A bicefalia urbana.\n Qual dessas opções está correta: A) 3 e 4 \n B) 1 e 2 \n C) 1, 3 e 4 \n D) 1, 2 e 4 ") 
 
 resposta3 = input ("Resposta: ")
 
@@ -33,10 +33,10 @@ if resposta3 == "B":
     score = score+1
 else: print("Você errou!")
 
-print("PERGUNTA 4 - André tem 1 carrinho, seu aniversário foi ontem e ele ganhou mais 2 carrinhos. Com quantos carrinhos André ficou? \n A) 1 carrinho  \n B) 0 carrinhos \n C) 5 carrinhos  \n D) 3 carrinhos \n") 
+print("PERGUNTA 4 - “Não existe geração de energia sem impacto ambiental. Esse impacto só será reduzido, se diminuirmos o consumo”, ressalta o pesquisador da Faculdade de Engenharia Mecânica da Unicamp, Gilberto Januzzi, em matéria publicada em 12/12/2004 no site http://www.comciencia.br. \n Dentre as fontes de energia indicadas abaixo, assinale a opção que apresenta a fonte alternativa de menor impacto ambiental. \n A) Construção de pequenas centrais hidrelétricas (PCHs)  \n B) Construção de Usinas Térmicas que aproveitam a energia do Urânio e do Plutônio  \n C) Geração de Energia Eólica (a partir dos ventos)  \n D) Utilização de biomassa (bagaço de cana e biogás de lixo) \n") 
 resposta4 = input ("Resposta: ")
 
-if resposta4 == "D":
+if resposta4 == "C":
     print ("Você acertou!")
     score = score+1
 else: print("Você errou!")
